c2fc554ae6965d3a5dbebdcbe4bcf42ec8216f82
Stalius4 <deivydasrimgaila@gmail.com>
2024-02-20 15:03:55 +0000

    hidden folder with functionality added for project 1

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,45 +5,69 @@ import project3 from "../assets/images/thumbnail-project-3-large.webp"
 import project4 from "../assets/images/thumbnail-project-4-large.webp"
 import project5 from "../assets/images/thumbnail-project-5-large.webp"
 import project6 from "../assets/images/thumbnail-project-6-large.webp"
+import React, { useState } from 'react';
 function Projects() {
+  const [isHovered, setIsHovered] = useState(false);
 
+  const handleMouseOver = () => {
+    setIsHovered(true);
+    console.log(isHovered)
+  };
+
+  const handleMouseOut = () => {
+    setIsHovered(false);
+    console.log(isHovered)
+  };
 
 
 
     return (
-    <section className="projects-outer">
-        <section className="top-projects-contactMe">
+    <section className="projects-outer" >
+        <section className="top-projects-contactMe"     
+>
           <h1>Projects</h1>
           <button >CONTACT ME</button>
         </section>
-      <div class="project-cards">
+      <div className="project-cards">
         <div className="project-container">
-          <img src={project1} alt="project1"/>
+
+        {/* on mouse over display hidden content */}
+          <img src={project1} alt="project1" onMouseOver={handleMouseOver} className="project-container-img"
+      />
           <div className="project-name">DESIGN PORTFOLIO</div>
           <div className="project-languages">HTML CSS</div>
+
+          {/* hidden  content/////   on mouse out display none this div */}
+          <div className={isHovered? "show-content-inner": "hidden-content-outer"} onMouseOut={handleMouseOut} >
+            <div className="hidden-content-inner" onMouseOver={handleMouseOver}>
+              <button className="view-project-btn" >VIEW PROJECT</button>
+              <button className="view-code-btn">VIEW CODE</button>
+            </div>
+          </div>
         </div>
         <div className="project-container">
-          <img src={project2} alt="project2"/>
+          <img src={project2} alt="project2" className="project-container-img"/>
           <div className="project-name">E-LEARNING LANDING PAGE</div>
           <div className="project-languages">HTML CSS</div>
+          
         </div>
         <div className="project-container">
-          <img src={project3} alt="project3"/>
+          <img src={project3} alt="project3"  className="project-container-img"/>
           <div className="project-name">TODO WEB APP</div>
           <div className="project-languages">HTML CSS JAVASCRIPT</div>
         </div>
         <div className="project-container">
-          <img src={project4} alt="project4"/>
+          <img src={project4} alt="project4" className="project-container-img"/>
           <div className="project-name">ENTERTAINMENT WEB APP</div>
           <div className="project-languages">HTML CSS JAVASCRIPT</div>
         </div>
         <div className="project-container">
-          <img src={project5} alt="project5"/>
+          <img src={project5} alt="project5" className="project-container-img"/>
           <div className="project-name">MEMORY GAME</div>
           <div className="project-languages">HTML CSS JAVASCRIPT</div>
         </div>
         <div className="project-container">
-          <img src={project6} alt="project6"/>
+          <img src={project6} alt="project6" className="project-container-img"/>
           <div className="project-name">ART GALLERY SHOWCASE</div>
           <div className="project-languages">HTML CSS JAVASCRIPT</div>
         </div>                                        
